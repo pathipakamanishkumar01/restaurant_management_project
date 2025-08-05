02be4b93f0a5f4d0f5c21d91fea41dc516c16763
Pathipaka Manish Kumar <54712142+pathipakamanishkumar01@users.noreply.github.com>
2025-08-06 03:35:56 +0530

    17849

--- account/models.py
+++ account/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from django.contrib..auth.user import User 
+from django.contrib.auth.user import User 
 
 class UserProfile(models.Model):
-    user = models.OneToOneFeild(User, on_delete=models.CASCADE)
-    name = models.CharFeild(max_length=255)
-    email = models.EmailFeild(unique=True)
-    phone_number = models.CharFeild(max_length=20, blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=20, blank=True)
 
     def __str__(self):
         return self.name or self.user.username
